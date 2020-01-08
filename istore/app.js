9786d75f3543579e160baeaab103408e573277d6
Canh Toan <nguyencanhtoan96@gmail.com>
2020-01-08 22:01:49 +0700

    change mail

--- istore/app.js
+++ istore/app.js
@@ -53,8 +53,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
 
-// let mongoDBUri = process.env.NODE_ENV === 'production' ? 'mongoDBUri' : 'localMongoDBUri';
-let mongoDBUri = 'mongoDBUri';
+let mongoDBUri =
+    process.env.NODE_ENV === 'production' ? 'mongoDBUri' : 'localMongoDBUri';
+//let mongoDBUri = 'mongoDBUri';
 // Connect to MongoDB
 mongoose.connect(config.get(mongoDBUri), {
     // mongoose.connect(config.get('localMongoDBUri'), {
